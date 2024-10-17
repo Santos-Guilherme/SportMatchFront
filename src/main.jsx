f1f520995b115b178605bcf40a6b4c0991f750ba
Leonardo Gama <leonardopereira10@outlook.com.br>
2024-10-16 21:54:27 -0300

    Pagina de Login

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login'
 import App from './App'
+import './styles/fonts.css';
 
 
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,58 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 
 export default function Login() {
-    return (
-      <div>
-        <h1>login</h1>
+  const [active, setActive] = useState(false);
+
+  const handleToggle = () => {
+    setActive(!active);
+  };
+
+  return (
+    <div className='Login'>
+      <div className={`container ${active ? 'active' : ''}`} id="container">
+        <div className="form-container sign-up">
+          <form>
+            <h1>Registrar</h1>
+            <input type="text" placeholder="Usuário" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+            <button type="button">Registrar</button>
+          </form>
+        </div>
+
+        <div className="form-container sign-in">
+          <form>
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Senha" />
+            <button type="button">Login</button>
+            <br>
+            </br>
+            <a href="#">Esqueceu a Senha?</a>
+          </form>
+        </div>
+
+        <div className="toggle-container">
+          <div className="toggle">
+            <div className="toggle-panel toggle-left">
+              <h1>Bem Vindo, de Volta!</h1>
+              <p>Entre na sua conta</p>
+              <button className="hidden" onClick={handleToggle}>
+                Login
+              </button>
+            </div>
+            <div className="toggle-panel toggle-right">
+              <h1>Olá, Amigo!</h1>
+              <p>Registre-se no site para ter acesso a plataforma</p>
+              <button className="hidden" onClick={handleToggle}>
+                Registrar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
-  
-
-  
-
+    </div>
+  );
+}
